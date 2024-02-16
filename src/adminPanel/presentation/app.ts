b9bd4267ad716c7590fcac6650c3b46588c2fb63
vishvjit-at <vishvjit.vanar@avestatechnologies.com>
2024-02-16 11:33:30 +0530

    chore: implemented cors

--- src/adminPanel/presentation/app.ts
+++ src/adminPanel/presentation/app.ts
@@ -4,14 +4,17 @@ import { Router as homePageRouter } from "./routes/homePageRoutes";
 import { Router as suburb } from "./routes/suburbRoutes";
 import { Router as featureFlagRouter } from "../../featureFlagService/presentation/routes/featureFlagRoutes";
 import { Router as jobRouter } from "./routes/jobRoutes";
-import {Router as defaultConfig} from "./routes/defaultConfigRoutes"
-import {Router as revAgency} from './routes/revAgencyRoutes'
+import { Router as defaultConfig } from "./routes/defaultConfigRoutes";
+import { Router as revAgency } from "./routes/revAgencyRoutes";
+import cors from "cors";
+
 const app = express();
 
 const port = 3000;
 
 app.use(express.json());
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/user", userRouter);
@@ -19,8 +22,8 @@ app.use("/homePage", homePageRouter);
 app.use("/suburb", suburb);
 app.use("/feature-flag", featureFlagRouter);
 app.use("/job", jobRouter);
-app.use("/defaultConfig", defaultConfig)
-app.use("/revAgency",revAgency)
+app.use("/defaultConfig", defaultConfig);
+app.use("/revAgency", revAgency);
 
 app.get("/", (req, res) => res.json({ data: "hello" }));
 
